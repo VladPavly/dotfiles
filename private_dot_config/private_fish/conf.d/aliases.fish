a7ebf0b263c16011ddd61ed9092ece243dcd70d6
Vlad <pvlad1285@gmail.com>
2024-02-12 14:48:13 +0300

    feat: sketchybar

--- private_dot_config/private_fish/conf.d/aliases.fish
+++ private_dot_config/private_fish/conf.d/aliases.fish
@@ -1,1 +1,10 @@
 alias vim="nvim"
+alias v="nvim"
+
+alias l="ls"
+alias c="clear"
+
+alias obs="cd \"/Users/vlad/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian\""
+
+alias s="kitty +kitten ssh"
+
